6e453064e4d3c11beb503ff82921fcd64ae99259
Jhon Pimiento <jhonpimiento@MacBook-Air-de-Jhon.local>
2026-06-16 14:48:58 -0500

    fix: corregir template literals en db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,13 @@
-import mongoose from "mongoose"; /*impotamos la libreria*/
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {                               /*aqui hago el asyn await para que nos
-                                        ayude a esperar antes de continuar */
-        const coon = await mongoose. connect (process.env.MONGODB_URI);  
-        console.log ('MongoDB ya esta conectado: ${conn.connection.host}');
-    } catch (error) {               /*try y catch nos avisar si algo sale mal */
-        console.error('A acurrido un error conectando MondoDB: ${error.message}');
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        // Backticks ` ` para que ${} funcione como variable
+        console.log(`MongoDB ya esta conectado: ${conn.connection.host}`);
+    } catch (error) {
+        // Backticks ` ` aquí también
+        console.error(`A ocurrido un error conectando MongoDB: ${error.message}`);
         process.exit(1);
     }
 };
